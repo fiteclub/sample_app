ce0afb4b532094257cc056527fd0b254c456617c
fiteclub <mattf255@yahoo.com>
2020-11-03 17:45:09 -0500

    10.1.3 Post Exercises

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,6 +18,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                                 password_confirmation: "bar" } }
   
       assert_template 'users/edit'
+      assert_select "div.alert", text: "The form contains 4 errors."
     end
   end
 end
